974a271f7d60b7b646f0efdc63bcdc8a2ebd4e61
hikaru-webcamp <skyvader0524@gmail.com>
2021-06-02 11:44:59 +0000

    2nd

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope module: 'user' do
+    root :to => 'homes#top'
+
+    devise_for :users, only: [:sessions, :registrations]
+
+    resources :users, only: [:index, :show, :edit, :update] do
+      get 'out_confirm' => 'users#out_confirm'
+      patch 'out' => 'users#out'
+    end
+
+    resources :groups, only: [:index, :show, :new, :edit, :create, :update, :destroy] do
+      resources :group_users, only: [:create, :destroy] 
+        resources :posts, only: [:create, :destroy] do
+         resource :likes, [:create, :destroy]
+        end
+    end
+    
+  end
+
+  devise_for :admins, path: '/admin', only: [:sessions], controllers: { :sessions => 'admin/sessions'}
+  namespace :admin do
+    root :to => 'homes#top'
+    resources :users, only: [:index, :show, :edit, :update]
+  end
+  
 end
