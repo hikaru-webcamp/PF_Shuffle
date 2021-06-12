593e029897d7cd67a3712c0d3e4cf5d001bb6883
hikaru-webcamp <skyvader0524@gmail.com>
2021-06-12 06:41:17 +0000

    【add】サインアップ修正、イイネ機能追加、seedファイル記載追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
   #バリデーションの記述(空白禁止と文字制限)
   validates :name, presence: true, length: {maximum: 20, minimum: 2} 
   validates :email, presence: true
-  validates :introduction, length: {maximum: 50}
+  validates :introduction, length: {maximum: 160} #ツイッターの自己紹介文は160文字
 
   #退会したユーザーはログイン出来ない仕様にする。
   def active_for_authentication?
