1ae97b70e35fbd5e688a883de39d36d86acfc23d
hikaru-webcamp <skyvader0524@gmail.com>
2021-06-16 01:14:24 +0000

    homesコントローラ内容修正

--- app/controllers/user/homes_controller.rb
+++ app/controllers/user/homes_controller.rb
@@ -1,6 +1,6 @@
 class User::HomesController < ApplicationController
   def top
      @all_ranks = Group.find(GroupUser.group(:group_id).order('count(group_id) desc').limit(3).pluck(:group_id))
-     @admins = Admin.find(1,2,3)
+     @admins = Admin.first(3)
   end
 end
