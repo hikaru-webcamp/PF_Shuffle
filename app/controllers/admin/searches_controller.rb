d4b49ae19dded33601a40ac538a51290dbc4522d
hikaru-webcamp <skyvader0524@gmail.com>
2021-06-24 12:10:17 +0000

    タイトル追加

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -1,6 +1,6 @@
 class Admin::SearchesController < ApplicationController
-    @keyword = params[:word]
   def search
+    @keyword = params[:word]
     @users = User.looks(params[:word]).order(created_at: :desc).page(params[:page]).per(12)
   end
 end
