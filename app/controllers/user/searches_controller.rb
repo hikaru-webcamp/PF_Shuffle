cb60fd536f2ffcb77db3bcb92fbec0b60cc3bbd6
hikaru-webcamp <skyvader0524@gmail.com>
2021-07-06 12:32:48 +0000

    N+1問題修正、TOPページサイン時のログインボタン修正

--- app/controllers/user/searches_controller.rb
+++ app/controllers/user/searches_controller.rb
@@ -5,10 +5,10 @@ class User::SearchesController < ApplicationController
     if @range == "User"
       @users = User.looks(params[:word]).order(created_at: :desc).page(params[:page]).per(12)
     elsif @range == "Group"
-      @groups = Group.looks(params[:word]).order(created_at: :desc).page(params[:page]).per(12)
+      @groups = Group.looks(params[:word]).includes([:owner]).order(created_at: :desc).page(params[:page]).per(12)
     else
       @groups = Group.all.includes(:post)
-      @posts = Post.looks(params[:word]).includes([:group]).order(created_at: :desc).page(params[:page]).per(12)
+      @posts = Post.looks(params[:word]).order(created_at: :desc).page(params[:page]).per(12)
     end
   end
 end
